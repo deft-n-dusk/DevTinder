18339d17d05a1b3edf60446caea9d125ae094197
Sonal Tyagi <sonaltyagi.biz@gmail.com>
2025-04-19 15:24:24 +0530

    Logout & Profile edit API

--- src/routes/profileRouter.js
+++ src/routes/profileRouter.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const profileRouter = express.Router();
 const {userAuth} = require("../middlewares/auth.js")
+const { validateProfileEditData} = require("../utils/validation.js");
 
 
 
 // Get profile API
-profileRouter.get("/profile", userAuth, async (req, res) => {
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
 
     try {
      const user = await req.user;
@@ -15,6 +16,26 @@ profileRouter.get("/profile", userAuth, async (req, res) => {
      catch (err){
          res.status(400).send("Error logging in: " + err.message);
      }
+ })
+
+
+ //Profile edit API
+ profileRouter.patch("/profile/edit", userAuth, async(req, res) => {
+    try{
+       if(!validateProfileEditData(req)){
+        throw new Error("Invalid edit data");
+       } 
+       const loggedInUser = req.user;
+       Object.keys(req.body).forEach((key) => (loggedInUser[key] = req.body[key]));
+
+       //To save the new edited data of the user in the database
+       await loggedInUser.save();
+
+       res.send(`${loggedInUser.firstName}, Your profile was updated successfully`)
+    }
+    catch(err){
+        res.status(400).send("Error : " + err.message);
+    }
  })
 
 
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -18,6 +18,16 @@ const validateSignUpData = (req) => {
     }
 }
 
+const validateProfileEditData = (req) => {
+
+    const allowedEditFields = ["firstName", "lastName", "emailId", "skills", "age", "gender", "bio", "photoUrl"];
+
+    const isEditAllowed = Object.keys(req.body).every(field => allowedEditFields.includes(field));
+
+    return isEditAllowed;
+}
+
 module.exports = {
-    validateSignUpData
+    validateSignUpData,
+    validateProfileEditData
 }
